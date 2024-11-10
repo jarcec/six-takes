5e618c54f87e8bb76da3abd72f90e3ba83737fca
Jarek Jarcec Cecho <jarcec@jarcec.com>
2024-11-10 15:27:32 -0800

    Initial commit / default maven repository

---
+++ src/main/java/com/jarcec/games/sixtakes/App.java
@@ -0,0 +1,10 @@
+package com.jarcec.games.sixtakes;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
