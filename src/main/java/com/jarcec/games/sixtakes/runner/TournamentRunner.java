27cddd90fc8075c5a3d59ba91b7b161e5013f879
Jarek Jarcec Cecho <jarcec@jarcec.com>
2024-11-17 09:16:25 -0800

    Add additional brains to default tournament

--- src/main/java/com/jarcec/games/sixtakes/runner/TournamentRunner.java
+++ src/main/java/com/jarcec/games/sixtakes/runner/TournamentRunner.java
@@ -1,13 +1,15 @@
 package com.jarcec.games.sixtakes.runner;
 
+import com.jarcec.games.sixtakes.brain.HighestCardBrain;
+import com.jarcec.games.sixtakes.brain.LowestCardBrain;
 import com.jarcec.games.sixtakes.brain.RandomBrain;
 import com.jarcec.games.sixtakes.engine.Player;
 import com.jarcec.games.sixtakes.engine.Statistics;
 import com.jarcec.games.sixtakes.engine.Tournament;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class TournamentRunner {
 
@@ -38,20 +40,21 @@ public class TournamentRunner {
     System.out.println("Welcome to SixTakes Tournament!");
 
     // Players
-    Set<Player> players = Set.of(
-      new Player("A", new RandomBrain()),
-      new Player("B", new RandomBrain()),
+    List<Player> players = List.of(
+      new Player("A", new LowestCardBrain()),
+      new Player("B", new HighestCardBrain()),
       new Player("C", new RandomBrain()),
       new Player("D", new RandomBrain()),
       new Player("E", new RandomBrain()),
       new Player("F", new RandomBrain())
     );
 
-    Tournament tournament = new Tournament(players);
+    // Tournament
+    Tournament tournament = new Tournament(new HashSet<>(players));
     Statistics statistics = tournament.playTournament(500);
 
+    // Print tournament results
     for(Player player : players) {
-
       Histogram rankHistogram = new Histogram(List.of("1", "2", "3", "4", "5", "6"));
       Histogram pointsHistogram = new Histogram(List.of("00-10", "11-20", "21-30", "31-40", "41-50", "51-60", "61-70", "71-80", "81-90", "91-100", "101-110"));
       for(Statistics.Result result: statistics.getResults().get(player)) {
